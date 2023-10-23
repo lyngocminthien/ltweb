15ddcba1a8b7f1ba5e94ce93c8b90c819c8f971b
lyngocminhthien <lyngocminhthien@egmail.com>
2023-10-23 17:05:45 +0700

    updated header UI

--- index.php
+++ index.php
@@ -15,38 +15,44 @@ require "conn.php";
     <link rel="icon" type="image/x-icon" href="Image/Icon/logo_web.svg">
 
     <!-- Link fonts && icon -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
     <link rel="stylesheet" 0 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Link general link -->
+    <link rel="stylesheet" href="css/css_base/base.css">
+    <link rel="stylesheet" href="css/css_base/grid.css">
 
     <!-- Link css apple store -->
-    <link rel="stylesheet" href="Css/tgdd.css">
-    <link rel="stylesheet" href="Css/ShowRoom.css">
-    <link rel="stylesheet" href="Css/footer.css">
-    <link rel="stylesheet" href="Css/product.css">
-    <link rel="stylesheet" href="Css/sign-up.css">
-    <link rel="stylesheet" href="Css/cart.css">
-    <link rel="stylesheet" href="Css/donhang.css">
+    <!-- css header -->
+    <link rel="stylesheet" href="css/header.css">
+
+    <link rel="stylesheet" href="css/tgdd.css">
+    <link rel="stylesheet" href="css/ShowRoom.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/product.css">
+    <link rel="stylesheet" href="css/sign-up.css">
+    <link rel="stylesheet" href="css/cart.css">
+    <link rel="stylesheet" href="css/donhang.css">
 </head>
 
 <body>
     <!-- ------------------------ Apple Store ------------------------------- -->
     <main class="wrapper">
         <!-- ------------------------ Header ------------------------------- -->
-        <header class="header-main">
+        <header style="background-color: #f9f9f9;" class="header-main">
             <?php
             require "pages/header.php";
             ?>
         </header>
 
         <!-- ------------------------ Body ------------------------------- -->
-        <section class="main_apple-store">
+        <section class="main_apple-store grid wide">
             <?php
             if (isset($_GET["url"])) {
                 $p = $_GET["url"]; //pages/$p.".php"
                 require "repository/" . $p . ".php";
             } else {
-                require "repository/home.php";
+                require "pages/home.php";
             }
             ?>
         </section>
--- pages/header.php
+++ pages/header.php
@@ -1,1 +1,25 @@
-Đây là header !!!
+<header class="header grid wide">
+    <nav class="navigation">
+        <img src="Image/Icon/logo_web.svg" alt="logo-header" class="logo_apple-store">
+        <section class="nav-content">
+            <div class="nav-top">
+                <div class="nav-top_search">
+                    <input type="text" name="input-search" id="input-search" placeholder="Tìm kiếm trên applestore.com">
+                    <button type="submit" name="submit-search">
+                        <img src="Image/Icon/search-icon.svg" alt="search-icon">
+                    </button>
+                </div>
+            </div>
+            <div class="nav-bottom">
+                <ul class="nav-list">
+                    <li class="nav-item"><a href="" class="nav-item-link">IPhone</a></li>
+                    <li class="nav-item"><a href="" class="nav-item-link">Mac</a></li>
+                    <li class="nav-item"><a href="" class="nav-item-link">IPad</a></li>
+                    <li class="nav-item"><a href="" class="nav-item-link">Watch</a></li>
+                    <li class="nav-item"><a href="" class="nav-item-link">AirPods</a></li>
+                </ul>
+            </div>
+        </section>
+        <a href=""><img src="Image/Icon/cart-icon.svg" alt="cart-icon"></a>
+    </nav>
+</header>
